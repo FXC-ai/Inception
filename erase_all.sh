0a638d99307d165fd7a5409765af9d8a18a71947
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-01-27 18:11:35 +0100

    Best version so far.

--- erase_all.sh
+++ erase_all.sh
@@ -8,4 +8,6 @@ docker container rm -f $(docker container ls -a -q)
 
 docker image rm -f $(docker image ls -a -q)
 
+docker volume rm -f $(docker volume ls -q)
+
 echo "All containers and images are removed"
--- srcs/requirements/mariadb/tools/start_mdb.sh
+++ srcs/requirements/mariadb/tools/start_mdb.sh
@@ -7,20 +7,30 @@ chown -R mysql:mysql /var/lib/mysql;
 chown -R mysql:mysql /run/mysqld;
 
 # launch mysqld in background
+echo "Lauching mariadb in background;"
 mysqld --user=mysql --datadir=/var/lib/mysql &
 
 pid=$!
 
 
-sleep 10
+sleep 5
 
 #Setting up the database
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 
-mysql -u root -p'mypass' -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'mypass';"
-mysql -u root -p'mypass' -e "CREATE DATABASE IF NOT EXISTS 'db_wordpress';"
-mysql -u root -p'mypass' -e "CREATE USER IF NOT EXISTS 'fcoindre' IDENTIFIED BY 'mypass';"
-mysql -u root -p'mypass' -e "GRANT ALL PRIVILEGES ON *.* TO 'fcoindre';"
-mysql -u root -p'mypass' -e "FLUSH PRIVILEGES;"
+echo "CREATE DATABASE IF NOT EXISTS;"
+mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DB_NAME};"
+
+echo "CREATE USER IF NOT EXISTS '${MARIADB_USER}' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}' IDENTIFIED BY '${MARIADB_PASS}';"
+
+echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}';"
+mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}';"
+
+echo "FLUSH PRIVILEGES;"
+mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
 
 # kill mysqld
 kill "$pid"
