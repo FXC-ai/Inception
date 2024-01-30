e4ebe2d1e6abf282524bd659bd1b30ad638e826c
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-01-30 17:39:50 +0100

    cli command for wordpress in progress

--- srcs/requirements/wordpress/tool/start_wdp.sh
+++ srcs/requirements/wordpress/tool/start_wdp.sh
@@ -3,6 +3,11 @@ echo "------------------------------- WORDPRESS START --------------------------
 php-fpm7.3 -v
 
 
+if [ -e /var/www/wordpress/wp-config.php]
+then echo "wp-config existe."
+else echo "wp-config existe pas."
+fi
+
 
 php-fpm7.3 -F
 
