fb169591c1a17f0fabcc7543858dc56712aa3371
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-02-12 23:49:31 +0100

    improvements post correction

--- InceptionForCorrection/srcs/requirements/nginx/tool/start_ngx.sh
+++
@@ -1,9 +0,0 @@
-echo "------------------------------- NGINX START -----------------------------------\n"
-
-nginx -v
-
-#force to run nginx in foreground in order to keep the container alive
-nginx -g 'daemon off;'
-
-
-
